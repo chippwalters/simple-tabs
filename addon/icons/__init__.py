d601c7679b24104148c4a570454f5bc613919baf
bonjorno7 <jorijndegraaf@gmail.com>
2020-12-04 16:21:27 +0100

    Add icons submodule
    This module is an exact copy paste of what I use everywhere else, because it works well.
    I'm also including a placeholder icon for SIMPLE TABS, because it's either that or starting with no icon, which would require code changes down the line and that's just extra hassle.

---
+++ addon/icons/__init__.py
@@ -0,0 +1,26 @@
+import bpy
+import bpy.utils.previews
+import pathlib
+
+
+pcoll = None
+
+
+def id(identifier: str) -> bpy.types.ImagePreview:
+    global pcoll
+    return pcoll[identifier].icon_id
+
+
+def register():
+    global pcoll
+    pcoll = bpy.utils.previews.new()
+
+    directory = pathlib.Path(__file__).parent
+
+    for filepath in directory.glob('*.png'):
+        pcoll.load(filepath.stem, str(filepath), 'IMAGE')
+
+
+def unregister():
+    global pcoll
+    bpy.utils.previews.remove(pcoll)
