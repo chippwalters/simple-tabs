4274486cb33b3a72ff227ee584b32a5538f1134f
bonjorno7 <jorijndegraaf@gmail.com>
2020-12-06 18:44:15 +0100

    Improve sidebar utilities
    I've separated the code that gathers panels into its own function, which now makes sure that only panels that are actually registered get through.

--- addon/utils/sidebar.py
+++ addon/utils/sidebar.py
@@ -1,14 +1,17 @@
 import bpy
 
 
-def categories() -> set:
-    categories = set()
+def panels() -> set:
+    panels = set()
 
     for panel in bpy.types.Panel.__subclasses__():
         if getattr(panel, 'bl_space_type', None) != 'VIEW_3D':
             continue
 
         if getattr(panel, 'bl_region_type', None) != 'UI':
+            continue
+
+        if not hasattr(bpy.types, panel.__name__):
             continue
 
         if hasattr(panel, 'poll'):
@@ -19,9 +22,18 @@ def categories() -> set:
                 continue
 
         if hasattr(panel, 'bl_category'):
-            if panel.bl_category:
-                categories.add(panel.bl_category)
-            else:
-                categories.add('Misc')
+            panels.add(panel)
+
+    return panels
+
+
+def categories() -> set:
+    categories = set()
+
+    for panel in panels():
+        if panel.bl_category:
+            categories.add(panel.bl_category)
+        else:
+            categories.add('Misc')
 
     return categories
