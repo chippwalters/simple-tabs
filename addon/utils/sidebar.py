7c6b72e3e45ce550aa0d4abdf4f06198698f6abe
bonjorno7 <jorijndegraaf@gmail.com>
2020-12-06 23:30:12 +0100

    Improve sidebar utility functions
    Bit of a vague commit name, let me elaborate:
    Added a dictionary for converting the current context.mode to the format used by bl_context.
    Moved panel checking code to its own function and implemented a bl_context check.
    Moved category check to its own function (this returns the panel's bl_category if there is one, otherwise "Misc", because that's where Blender puts those).
    Also the category check now uses getattr, just in case a panel manages to get through the panel check and not have a bl_category (rare but possible). This also takes into account values of None or empty string.

--- addon/utils/sidebar.py
+++ addon/utils/sidebar.py
@@ -1,37 +1,73 @@
 import bpy
 
 
+mode_table = {
+    'EDIT_MESH': 'mesh_edit',
+    'EDIT_CURVE': 'curve_edit',
+    'EDIT_SURFACE': 'surface_edit',
+    'EDIT_TEXT': 'text_edit',
+    'EDIT_ARMATURE': 'armature_edit',
+    'EDIT_METABALL': 'mball_edit',
+    'LATTICE_EDIT': 'lattice_edit',
+    'POSE': 'posemode',
+    'SCULPT': 'sculpt_mode',
+    'PAINT_WEIGHT': 'weightpaint',
+    'PAINT_VERTEX': 'vertexpaint',
+    'PAINT_TEXTURE': 'imagepaint',
+    'PARTICLE': 'particlemode',
+    'OBJECT': 'objectmode',
+    'PAINT_GPENCIL': 'greasepencil_paint',
+    'EDIT_GPENCIL': 'greasepencil_edit',
+    'SCULPT_GPENCIL': 'greasepencil_sculpt',
+    'WEIGHT_GPENCIL': 'greasepencil_weight',
+    'VERTEX_GPENCIL': 'greasepencil_vertex',
+}
+
+
+def check(panel: bpy.types.Panel) -> bool:
+    if getattr(panel, 'bl_space_type', None) != 'VIEW_3D':
+        return False
+
+    if getattr(panel, 'bl_region_type', None) != 'UI':
+        return False
+
+    if getattr(panel, 'bl_context', None):
+        bl_context = panel.bl_context.replace('.', '')
+        if bl_context != mode_table[bpy.context.mode]:
+            return False
+
+    if hasattr(panel, 'poll'):
+        try:
+            if not panel.poll(bpy.context):
+                return False
+        except:
+            return False
+
+    return True
+
+
 def panels() -> set:
     panels = set()
 
     for panel in bpy.types.Panel.__subclasses__():
-        if getattr(panel, 'bl_space_type', None) != 'VIEW_3D':
-            continue
-
-        if getattr(panel, 'bl_region_type', None) != 'UI':
-            continue
-
-        if hasattr(panel, 'poll'):
-            try:
-                if not panel.poll(bpy.context):
-                    continue
-            except:
-                continue
-
-        if hasattr(panel, 'bl_category'):
+        if check(panel):
             panels.add(panel)
 
     return panels
+
+
+def category(panel: bpy.types.Panel) -> str:
+    if getattr(panel, 'bl_category', None):
+        return panel.bl_category
+    else:
+        return 'Misc'
 
 
 def categories() -> set:
     categories = set()
 
     for panel in panels():
-        if panel.bl_category:
-            categories.add(panel.bl_category)
-        else:
-            categories.add('Misc')
+        categories.add(category(panel))
 
     return categories
 
