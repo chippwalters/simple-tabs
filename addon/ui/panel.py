c11f0eed46c542fe2ee2af6a43c140abfa1d39d2
bonjorno7 <jorijndegraaf@gmail.com>
2020-12-18 03:30:23 +0100

    Add new UI to panel
    I'm making the UIList have the length of the list of tabs, because I think it looks nice and works well.
    The buttons are on the side, as with almost all UILists in Blender.
    I made them 1.5 times as big (at reduced width so they remain square) because I think it looks better, and is easier to click on.
    I like the icons I chose for the buttons, but I can change them if you have better ones in mind.

--- addon/ui/panel.py
+++ addon/ui/panel.py
@@ -20,6 +20,19 @@ class MainPanel(bpy.types.Panel):
         if self.is_popover:
             layout.ui_units_x = prefs.popover_width
 
+        row = column.row()
+        row.template_list("SIMPLETABS_UL_TabList", '', prefs, 'tab_items', prefs, 'tab_index', rows=len(prefs.tab_items))
+
+        col = row.column(align=True)
+        col.scale_x = 1.25
+        col.scale_y = 1.5
+
+        col.operator('simpletabs.refresh', text='', icon='FILE_REFRESH')
+        col.separator()
+        col.operator('simpletabs.move', text='', icon='TRIA_UP').direction = 'UP'
+        col.operator('simpletabs.move', text='', icon='TRIA_DOWN').direction = 'DOWN'
+        col.separator()
+        col.operator('simpletabs.update', text='', icon='CHECKMARK')
 
 
 def popover(self, context: bpy.types.Context):
