98f950b691c56f35c5fd1b609f9cab6418bbfcad
bonjorno7 <jorijndegraaf@gmail.com>
2021-01-02 22:48:04 +0100

    First attempt at updating sidebar on startup
    Everything works except for BoxCutter.
    Which does work if you manually press the update button btw.
    I'm going with 5 seconds of delay because it seems plenty long enough (and not too long either), but we might have to increase it or make it customizable in the future.

--- addon/__init__.py
+++ addon/__init__.py
@@ -18,6 +18,13 @@ def register():
     for module in modules:
         module.register()
 
+    import bpy
+
+    def timer():
+        bpy.ops.simpletabs.update()
+
+    bpy.app.timers.register(timer, first_interval=5)
+
 
 def unregister():
     for module in reversed(modules):
