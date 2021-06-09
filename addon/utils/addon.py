99de5f41bbd1d4d97e2e42968345acff711f6525
bonjorno7 <jorijndegraaf@gmail.com>
2021-06-09 17:32:09 +0200

    Add popup function to show information
    With this, we can easily show important information in a little popup.
    It has three types: INFO, WARNING, and ERROR.

--- addon/utils/addon.py
+++ addon/utils/addon.py
@@ -8,3 +8,13 @@ def module() -> str:
 def prefs() -> bpy.types.AddonPreferences:
     addons = bpy.context.preferences.addons
     return addons[module()].preferences
+
+
+def popup(type: str, message: str):
+    def report(self: bpy.types.Menu, context: bpy.types.Context):
+        self.layout.emboss = 'NONE'
+        self.layout.label(text=message)
+
+    title = f'SIMPLE TABS {type}'
+    icon = {'INFO': 'INFO', 'WARNING': 'ERROR', 'ERROR': 'CANCEL'}[type]
+    bpy.context.window_manager.popup_menu(report, title, icon)
