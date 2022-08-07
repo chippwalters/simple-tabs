4bb85b07df05bf95d7c687c94283b8bac1ff6eba
bonjorno7 <jorijndegraaf@gmail.com>
2022-08-08 00:03:50 +0200

    Adjust minimum list length based on move buttons
    When the move buttons aren't visible, 8 items is a little long, so I'm shrinking it to 6.

--- addon/ui/panel.py
+++ addon/ui/panel.py
@@ -13,6 +13,7 @@ class MainPanel(bpy.types.Panel):
 
     def draw(self, context: bpy.types.Context):
         prefs = utils.addon.prefs()
+        show_move = len(prefs.tab_items) >= 2
 
         layout = self.layout
         column = layout.column()
@@ -21,7 +22,7 @@ class MainPanel(bpy.types.Panel):
             layout.ui_units_x = prefs.popover_width
 
         row = column.row()
-        rows = max(len(prefs.tab_items), 8)
+        rows = max(len(prefs.tab_items), 8 if show_move else 6)
         row.template_list("SIMPLETABS_UL_TabList", '', prefs, 'tab_items', prefs, 'tab_index', rows=rows)
 
         col = row.column(align=True)
@@ -33,7 +34,7 @@ class MainPanel(bpy.types.Panel):
         col.operator('simpletabs.add', text='', icon='ADD')
         col.operator('simpletabs.remove', text='', icon='REMOVE')
         col.separator()
-        if len(prefs.tab_items) >= 2:
+        if show_move:
             col.operator('simpletabs.move', text='', icon='TRIA_UP').direction = 'UP'
             col.operator('simpletabs.move', text='', icon='TRIA_DOWN').direction = 'DOWN'
             col.separator()
